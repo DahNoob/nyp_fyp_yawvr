e734eb9e4aa5d682173932de750483b630d1d1c5
Lee Wei Hao <multimac@hotmail.com>
2019-12-13 14:43:33 +0800

    minor update because there's an error in my script

--- YawVR_Oculus_Quest/Assets/Scripts/Wei Hao/Enemy/Light_Enemy_1.cs
+++ YawVR_Oculus_Quest/Assets/Scripts/Wei Hao/Enemy/Light_Enemy_1.cs
@@ -11,11 +11,12 @@ public class Light_Enemy_1 : EnemyBase
         DIE,
     }
 
+    public Transform projectile;
+    private Transform target;
+
     private GameObject Player;
-    // Chase range
-    private int chaseRange = 10;
     // Attack range
-    private int attackRange = 4;
+    private int attackRange = 10;
     // Baneling rotation speed
     private int rotationSpeed = 5;
     // Time taken before attack is activated
@@ -41,12 +42,13 @@ public class Light_Enemy_1 : EnemyBase
     // Start is called before the first frame update
     void Start()
     {
-        maxHealth = 1;
+        maxHealth = 50;
         health = maxHealth;
-        damage = 10;
+        damage = 5;
         moveSpeed = 6;
         currentState = _EnemyState.CHASE;
         Player = GameObject.Find("Player");
+        target = Player.GetComponent<Transform>();
         rb = gameObject.GetComponent<Rigidbody>();
         m_Animator = gameObject.GetComponentInChildren<Animator>();
         poof = gameObject.GetComponent<ParticleSystem>();
@@ -68,7 +70,7 @@ public class Light_Enemy_1 : EnemyBase
         if (Vector3.Distance(transform.position, Player.transform.position) >= attackRange)
         {
             currentState = _EnemyState.CHASE;
-            m_Animator.SetBool("Chase", true);
+            //m_Animator.SetBool("Chase", true);
 
             Debug.Log("Current State: " + currentState);
             Debug.Log("Distance: " + distance);
@@ -83,37 +85,36 @@ public class Light_Enemy_1 : EnemyBase
 
         switch (currentState)
         {
-            case _EnemyState.WALK:
+            case _EnemyState.CHASE:
                 Quaternion toRotation = Quaternion.LookRotation(new Vector3(relativePos.x, 0, relativePos.z));
                 transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
 
                 transform.position += transform.forward * moveSpeed * Time.deltaTime;
                 break;
             case _EnemyState.SHOOT:
-                attackWindUp -= 1.0f * Time.deltaTime;
 
-                transformX.x = Mathf.Sin(Time.time * speed) * amount;
+                Instantiate(projectile,transform.position + (target.position - transform.position).normalized, Quaternion.LookRotation(target.position - transform.position));
+                //attackWindUp -= 1.0f * Time.deltaTime;
+                //if(attackWindUp <= 0.0f)
+                //{
 
-                if (attackWindUp <= 0.0f)
-                {
-                    Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, 5.0f);
-                    for (int i = 0; i < hitColliders.Length; i++)
-                    {
-                        if (hitColliders[i].gameObject.tag == "Player")
-                        {
-                            //ImpactReceiver IR = hitColliders[i].transform.GetComponent<ImpactReceiver>();
-                            //Vector3 dir = hitColliders[i].transform.position - transform.position;
-                            //float force = Mathf.Clamp(/*explosion force*/ 50 / 3, 0, 15); 
-                            //IR.AddImpact(dir, force);
-                            //Player.gameObject.GetComponent<Player>().takeDamage(damage);  
-                            //poof.Play();
-                            Debug.Log("B00M !!");
-                            //Debug.Log("Player health = " + Player.GetComponent<Player>().GetHealth());
-                        }
-                    }
-                    //PlayDeathParticle();
-                    gameObject.SetActive(false);
-                }
+                //}
+
+
+                //transformX.x = Mathf.Sin(Time.time * speed) * amount;
+
+                //if (attackWindUp <= 0.0f)
+                //{
+                //    Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, 5.0f);
+                //    for (int i = 0; i < hitColliders.Length; i++)
+                //    {
+                //        if (hitColliders[i].gameObject.tag == "Player")
+                //        {
+                //            Debug.Log("B00M !!");
+                //        }
+                //    }
+                //gameObject.SetActive(false);
+                //}
                 break;
             case _EnemyState.DIE:
                 break;
@@ -141,7 +142,7 @@ public class Light_Enemy_1 : EnemyBase
         if (collision.gameObject.tag == "Bullet")
         {
             takeDamage(1);
-            //collision.gameObject.SetActive(false);
+            collision.gameObject.SetActive(false);
         }
     }
 }
