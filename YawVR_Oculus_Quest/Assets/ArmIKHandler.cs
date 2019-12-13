37d4c8e42f0be27699bfa5db248fef1537d57fbb
dah@nyp <valencoen222@gmail.com>
2019-12-13 16:04:50 +0800

    tryin out a "fake" ik arm lululul

---
+++ YawVR_Oculus_Quest/Assets/ArmIKHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmIKHandler : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]
+    private Animator m_armModel;
+    [SerializeField]
+    private Transform m_handTransform;
+    [SerializeField]
+    private OVRInput.Controller m_controller;
+
+    [Header("Debugs")]
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float m_armStretch = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //2.55
+        float dist = (transform.position - m_handTransform.position).magnitude;
+        m_armStretch = dist / 2.55f;
+        m_armModel.SetFloat("Blend", m_armStretch);
+        transform.LookAt(m_handTransform);
+    }
+}
