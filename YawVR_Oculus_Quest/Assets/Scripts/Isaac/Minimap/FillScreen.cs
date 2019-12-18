41730fb1dcb28a0e2ebb4b87eea7deb33cb75868
isaacchunn <isaacchunn@hotmail.com>
2019-12-18 18:10:06 +0800

    - Horrible minimap test, gave up on the shaders for now since no time - Some test textures are in my scripts folders, but shouldn't affect anything

---
+++ YawVR_Oculus_Quest/Assets/Scripts/Isaac/Minimap/FillScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillScreen : MonoBehaviour
+{
+    //The camera to be used as a reference
+    [SerializeField]
+    private Camera cam;
+    [SerializeField]
+    private float distFromCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+            float pos = (cam.nearClipPlane + distFromCamera);
+
+            transform.position = cam.transform.position + cam.transform.forward * pos;
+
+            float h = Mathf.Tan(cam.fieldOfView * Mathf.Deg2Rad * 0.5f) * pos * 2f;
+
+            transform.localScale = new Vector3(h * cam.aspect, h, 0f);
+        
+    }
+}
---
+++ YawVR_Oculus_Quest/Assets/Scripts/Isaac/Minimap/MinimapFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapFollow : MonoBehaviour
+{
+    public enum FOLLOW_MODES
+    {
+
+    };
+    [Header("Minimap Configurations")]
+    [SerializeField]
+    private GameObject objectToFollow;
+    [SerializeField]
+    private float heightOffset = 5f;
+
+    [Header("Follow Configurations")]
+    [SerializeField]
+    FOLLOW_MODES followModes;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = objectToFollow.transform.position;
+        transform.position = new Vector3(targetPosition.x, targetPosition.x + heightOffset, targetPosition.z);
+
+        Quaternion targetRotation = objectToFollow.transform.rotation;
+        transform.rotation = Quaternion.Euler(90f, targetRotation.eulerAngles.y, 0);
+
+    }
+}
