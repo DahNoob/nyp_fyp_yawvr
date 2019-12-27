cce53cc957cf8e36b3217d8fadc31ea0408033e0
Lee Wei Hao <multimac@hotmail.com>
2019-12-27 11:55:10 +0800

    - Revamped and edited LODs, hopefully it is less obvious now - Changed some options in quality settings and pipelines to prevent the shadwos from flickering - Added some test script to visualize the LODs next time if there is ever a need to change

---
+++ YawVR_Oculus_Quest/Assets/Prefabs/LODSet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LODSet : MonoBehaviour
+{
+    public Material[] lodMats;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = transform.GetChild(0).gameObject;
+        for(int i =0; i < target.transform.childCount; i++)
+        {
+            Transform t = target.transform.GetChild(i);
+            t.gameObject.GetComponent<MeshRenderer>().material = lodMats[i];
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
