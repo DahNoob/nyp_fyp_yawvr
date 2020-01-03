914b0b59b8bd62384ebe410b9f62618d65dfb83d
Lee Wei Hao <multimac@hotmail.com>
2020-01-03 18:08:41 +0800

    qweqwewq

--- YawVR_Oculus_Quest/Assets/FM2_Idle.cs
+++ YawVR_Oculus_Quest/Assets/FM2_Idle.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class FM2_Idle : SMB_BaseEnemyState
 {
+    [Header("Enemy Walk State Configuration")]
+    [SerializeField]
+    protected float m_detectRange = 50.0f;
+    [SerializeField]
+    protected float m_fieldOfViewAngle = 60.0f;
+
     public override void Check(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
-        
+        PlayerHandler player = PlayerHandler.instance;
+        animator.SetBool("Chase_InRange", CustomUtility.IsHitRadius(player.transform.position, enemy.transform.position, m_detectRange));
     }
 
     public override void Enter(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
-        
-    }
-
-    public override void Exit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
-    {
-        
+        animator.SetBool("Chase_InRange", false);
     }
 
     public override void Update(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
+    {
+
+    }
+
+    public override void Exit(Animator animator, AnimatorStateInfo animatorStateInfo, int layerIndex)
     {
         
     }
