b1d84a9247873a90ca069ab8ee77bf49a3f5e0b6
isaacchunn <isaacchunn@hotmail.com>
2020-01-07 14:22:18 +0800

    - Fix the projectile

--- YawVR_Oculus_Quest/Assets/Scripts/Isaac/QuadTree/TerrainObject.cs
+++ YawVR_Oculus_Quest/Assets/Scripts/Isaac/QuadTree/TerrainObject.cs
@@ -7,7 +7,7 @@ public class TerrainObject : StaticQuadTreeObject
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Adding " + gameObject.name + " to static tree");
+        //Debug.Log("Adding " + gameObject.name + " to static tree");
         AddToQuadTree(this.gameObject,QuadTreeManager.STATIC_TYPES.TERRAIN);
     }
 
