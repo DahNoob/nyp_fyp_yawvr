97bc3ad28464a1f3f856a4eee13ad238ab40d70f
dah@nyp <valencoen222@gmail.com>
2020-01-15 16:46:00 +0800

    updates to laserblaster

--- YawVR_Oculus_Quest/Assets/Scripts/Weapons/BaseClasses/MechGunWeapon.cs
+++ YawVR_Oculus_Quest/Assets/Scripts/Weapons/BaseClasses/MechGunWeapon.cs
@@ -19,6 +19,10 @@ abstract public class MechGunWeapon : MechBaseWeapon
     protected AudioClip[] m_shootAudioClips;
     [SerializeField]
     protected MechLaserPointer m_laserPointer;
+    [SerializeField]
+    protected Vector3 m_recoilPosition = new Vector3(0, 0.02f, 0.05f);
+    [SerializeField]
+    protected Vector3 m_recoilRotation = new Vector3(-2, 0, 0);
 
     [Header("UI Configuration")]
     //Current weapon ammo
@@ -28,6 +32,9 @@ abstract public class MechGunWeapon : MechBaseWeapon
     [Header("Ammo Configuration")]
     [SerializeField]
     protected AmmoModule ammoModule;
+
+    //Local variables
+    protected float shootTick;
 
     virtual protected void Start()
     {
@@ -44,10 +51,58 @@ abstract public class MechGunWeapon : MechBaseWeapon
         return true;
     }
 
+    public override bool Hold(OVRInput.Controller _controller)
+    {
+        //If the ammo module is not reloading?
+        if (ammoModule.m_isReloading == false)
+        {
+            shootTick += Time.deltaTime;
+            if (shootTick >= m_shootInterval)
+            {
+                shootTick -= m_shootInterval;
+                //if (PlayerHandler.instance.DecreaseEnergy(m_energyReduction))
+                //{
+                if (ammoModule.DecreaseAmmo(1))
+                {
+                    //Set the current ammo count
+                    weaponAmmoText.text = ammoModule.currentAmmo.ToString();
+                    SpawnProjectile();
+                    Vibe();
+                    follower.Bump(m_recoilPosition, m_recoilRotation);
+                    //VibrationManager.SetControllerVibration(m_controller, vibeClip);
+                    m_shootAudioSource.clip = m_shootAudioClips[Random.Range(0, m_shootAudioClips.Length - 1)];
+                    m_shootAudioSource.Play();
+
+                    //Triggered
+                    GUIManager.instance.Triggered(_controller);
+                    return true;
+                }
+                else
+                {
+                    //Try to reload? I mean it's a test.
+                    StartCoroutine(ammoModule.Reload());
+                }
+            }
+        }
+        return false;
+    }
+
     override public bool Unselected()
     {
         base.Unselected();
         mechHand.SetPose("HoldGun", false);
         return true;
+    }
+
+    virtual protected void SpawnProjectile()
+    {
+        BaseProjectile derp = Instantiate(m_projectilePrefab, m_projectileOrigin.position, m_projectileOrigin.rotation, Persistent.instance.GO_DYNAMIC.transform).GetComponent<BaseProjectile>();
+        derp.Init(m_projectileOrigin);
+    }
+
+    //vibecheck dawg
+    virtual protected void Vibe()
+    {
+        VibrationManager.SetControllerVibration(m_controller, 0.01f, 0.4f);
     }
 }
--- YawVR_Oculus_Quest/Assets/Scripts/Weapons/LaserGunWeapon.cs
+++ YawVR_Oculus_Quest/Assets/Scripts/Weapons/LaserGunWeapon.cs
@@ -8,13 +8,6 @@ public class LaserGunWeapon : MechGunWeapon
     [Header("Laser Gun Configuration")]
     [SerializeField]
     protected Animator m_gatlingAnimator;
-    [SerializeField]
-    protected Vector3 m_recoilPosition = new Vector3(0, 0.02f, 0.05f);
-    [SerializeField]
-    protected Vector3 m_recoilRotation = new Vector3(-2, 0, 0);
-
-    //Local variables
-    private float shootTick;
 
     public override bool Grip()
     {
@@ -33,44 +26,6 @@ public class LaserGunWeapon : MechGunWeapon
         }
         //m_laserPointer.gameObject.SetActive(true);
         return true;
-    }
-
-    public override bool Hold(OVRInput.Controller _controller)
-    {
-        //If the ammo module is not reloading?
-        if (ammoModule.m_isReloading == false)
-        {
-            shootTick += Time.deltaTime;
-            if (shootTick >= m_shootInterval)
-            {
-                shootTick -= m_shootInterval;
-                //if (PlayerHandler.instance.DecreaseEnergy(m_energyReduction))
-                //{
-                if (ammoModule.DecreaseAmmo(1))
-                {
-                    //Set the current ammo count
-                    weaponAmmoText.text = ammoModule.currentAmmo.ToString();
-
-                    BaseProjectile derp = Instantiate(m_projectilePrefab, m_projectileOrigin.position, m_projectileOrigin.rotation, Persistent.instance.GO_DYNAMIC.transform).GetComponent<BaseProjectile>();
-                    derp.Init(m_projectileOrigin);
-                    follower.Bump(m_recoilPosition, m_recoilRotation);
-                    //VibrationManager.SetControllerVibration(m_controller, vibeClip);
-                    VibrationManager.SetControllerVibration(m_controller, 0.01f, 0.4f);
-                    m_shootAudioSource.clip = m_shootAudioClips[Random.Range(0, m_shootAudioClips.Length - 1)];
-                    m_shootAudioSource.Play();
-
-                    //Triggered
-                    GUIManager.instance.Triggered(_controller);
-                    return true;
-                }
-                else
-                {
-                    //Try to reload? I mean it's a test.
-                    StartCoroutine(ammoModule.Reload());
-                }
-            }
-        }
-        return false;
     }
 
     public override bool Stop(OVRInput.Controller _controller)
