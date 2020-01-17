158f29cb279ce262c32654d511c1a48358cd5bef
isaacchunn <isaacchunn@hotmail.com>
2020-01-17 11:11:37 +0800

    - Kinda fixed object pooling, just i think the adding objects dynamically has abit of issues but it should work fine for now, if not I'll change to list

--- YawVR_Oculus_Quest/Assets/Scripts/TestShootiong.cs
+++ YawVR_Oculus_Quest/Assets/Scripts/TestShootiong.cs
@@ -5,17 +5,13 @@ using UnityEngine;
 public class TestShootiong : MonoBehaviour
 {
     public GameObject bulletPrefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
-    {   
+    {
         BaseProjectile derp = ObjectPooler.instance.SpawnFromPool(PoolObject.OBJECTTYPES.TEST_PROJECTILE, transform.position, transform.rotation).GetComponent<BaseProjectile>();
         //BaseProjectile derp = Instantiate(bulletPrefab, transform.position, transform.rotation, Persistent.instance.GO_DYNAMIC.transform).GetComponent<BaseProjectile>();
         derp.Init(transform);
     }
+
 }
