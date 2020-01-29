174027817640350d29ff3e96f77081cda6d78c75
Lee Wei Hao <multimac@hotmail.com>
2020-01-29 12:47:44 +0800

    - Fixed nav mesh for all enemies, expect the performance to take a hit. - Removed old nav mesh and clean up stuff - Add prop folder - Fix some saving issue with the mech prefab

--- YawVR_Oculus_Quest/Assets/Scripts/Game.cs
+++ YawVR_Oculus_Quest/Assets/Scripts/Game.cs
@@ -243,11 +243,11 @@ public class Game : MonoBehaviour
         for (int i = 0; i < MapPointsHandler.instance.m_mapPoints.Count; ++i)
         {
             bool loadloadaald = !allocatedPoints.Contains(i);
-            print("adsadasd " + loadloadaald);
+            //print("adsadasd " + loadloadaald);
             if(!allocatedPoints.Contains(i) && Random.Range(0.0f,100.0f) < m_enemySpawnProbability && !CustomUtility.IsHitRadius(MapPointsHandler.instance.m_mapPoints[i], PlayerHandler.instance.transform.position, m_enemySpawnDeadzone))
             {
                 EnemyBase enemy = ObjectPooler.instance.SpawnFromPool(m_enemies[Random.Range(0, m_enemies.Length)].poolType, MapPointsHandler.instance.m_mapPoints[i], Quaternion.identity).GetComponent<EnemyBase>();
-                print("Enemy deployed at point " + i);
+                //print("Enemy deployed at point " + i);
             }
         }
     }
