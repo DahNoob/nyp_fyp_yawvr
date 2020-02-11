c8517e61ac5b4cbb9ef4d9a8ced051c058710ac8
dah@nyp <valencoen222@gmail.com>
2020-02-11 17:50:59 +0800

    more progress on mainhub

--- YawVR_Oculus_Quest/Assets/Scripts/PlanetHolographs/BasePlanetHolograph.cs
+++ YawVR_Oculus_Quest/Assets/Scripts/PlanetHolographs/BasePlanetHolograph.cs
@@ -6,7 +6,34 @@ abstract public class BasePlanetHolograph : MonoBehaviour
 {
     [Header("Base Configuration")]
     [SerializeField]
-    protected GameObject m_holographPrefab;
+    public string m_sceneName;
+    [SerializeField]
+    protected Transform m_holographRoot;
+    [SerializeField]
+    public Color m_holographColor;
+    [SerializeField]
+    [Range(0.0f, 10.0f)]
+    protected float m_bobSpeed = 1;
+    [SerializeField]
+    [Range(0.0f, 0.25f)]
+    protected float m_bobDistance = 0.1f;
+    [SerializeField]
+    protected float m_rotationSpeed = 22.5f;
 
-    protected Transform holographRoot;
+    protected MeshRenderer[] meshes;
+
+    virtual protected void Awake()
+    {
+        meshes = GetComponentsInChildren<MeshRenderer>();
+        for (int i = 0; i < meshes.Length; ++i)
+        {
+            meshes[i].material.SetColor("_RimColor", m_holographColor);
+        }
+    }
+
+    virtual protected void Update()
+    {
+        m_holographRoot.localPosition = new Vector3(0, Mathf.Cos(Time.time * m_bobSpeed) * m_bobDistance, 0);
+        m_holographRoot.Rotate(0, m_rotationSpeed * Time.deltaTime, 0);
+    }
 }
